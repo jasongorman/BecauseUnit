58f5cef8044b2cb32975ee1b90ebc6d71d029201
Jason Gorman <jason.gorman@codemanship.com>
2016-03-13 10:49:58 +0000

    Oops. Didn't merge files correctly.

--- BecauseUnit/test/com/codemanship/becauseunit/BecauseParameterisedTest.java
+++ BecauseUnit/test/com/codemanship/becauseunit/BecauseParameterisedTest.java
@@ -7,23 +7,35 @@ import com.codemanship.becauseunit.doubles.SpyWriter;
 
 public class BecauseParameterisedTest {
 	
+	private SpyWriter writer;
+
 	public void becauseParameterTypesAreListed() throws NoSuchMethodException, SecurityException{
-		ParameterisedTest test = createParameterisedTest();
+		ParameterisedTest test = createParameterisedTest("parameterised");
 		check(test.getParameterTypes()[0].equals(String.class));
 	}
 	
 	public void becauseFindsDataForParameterisedTest() throws NoSuchMethodException, SecurityException{
-		ParameterisedTest test = createParameterisedTest();
+		ParameterisedTest test = createParameterisedTest("parameterised");
 		check(test.getData().length == datafor_parameterised().length);
 	}
 
 	
 	public void becauseRunningParameterisedTestInvokesTestMethodForEachSetOfData() throws NoSuchMethodException, SecurityException{
-		ParameterisedTest test = createParameterisedTest();
-		SpyWriter writer = new SpyWriter();
+		ParameterisedTest test = createParameterisedTest("parameterised");
+		runTest(test);
+		check(writer.getLastWrite().contains("passed"));	
+	}
+	
+	public void becauseFailsTestIfDataDoesntMatchParameters() throws NoSuchMethodException{
+		Test test = createParameterisedTest("parameterisedUnmatched");
+		runTest(test);
+		check(writer.getLastWrite().contains("failed"));
+	}
+
+	private void runTest(Test test) {
+		writer = new SpyWriter();
 		Output output = new ConsoleOutput(writer );
 		test.run(output);
-		check(writer.getLastWrite().contains("passed"));	
 	}
 	
 	public void parameterised(String param, boolean foo){
@@ -33,10 +45,18 @@ public class BecauseParameterisedTest {
 	public Object[][] datafor_parameterised(){
 		return new Object[][]{{"hello", true}};
 	}
+	
+	public void parameterisedUnmatched(String param, boolean foo){
+		check(param.equals("hello") && foo);
+	}
+	
+	public Object[][] datafor_parameterisedUnmatched(){
+		return new Object[][]{{"hello"}};
+	}
 
-	private ParameterisedTest createParameterisedTest()
+	private ParameterisedTest createParameterisedTest(String testMethodName)
 			throws NoSuchMethodException {
-		ParameterisedTest test = new ParameterisedTest(this, this.getClass().getDeclaredMethod("parameterised", String.class, boolean.class));
+		ParameterisedTest test = new ParameterisedTest(this, this.getClass().getDeclaredMethod(testMethodName, String.class, boolean.class));
 		return test;
 	}
 }
